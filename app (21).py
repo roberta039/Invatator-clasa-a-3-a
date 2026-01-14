5da6d2a9f3654bc30af31206374d892dd858dd84
roberta039 <165647793+roberta039@users.noreply.github.com>
2026-01-14 17:22:55 +0200

    Update app (21).py

--- app (21).py
+++ app (21).py
@@ -7,7 +7,7 @@ from io import BytesIO # <--- NOU: Pentru audio în memorie
 
 # 1. Configurare Pagină
 st.set_page_config(page_title="Doamna Învățătoare", page_icon="🧠")
-st.title(""🧠 Profesor Universal"")
+st.title(""🧠 Doamna Învățătoare"")
 
 # 2. Configurare API Key
 if "GOOGLE_API_KEY" in st.secrets:
